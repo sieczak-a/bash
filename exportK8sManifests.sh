69fcd793ff1b7c4d1960fee825433535d708117e
sieczaa <arkadiusz.sieczak@play.pl>
2021-11-22 22:57:41 +0100

    export manifests from k8s cluster

---
+++ exportK8sManifests.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# USAGE
+# chmod +x exportK8sManifests
+# ./exportK8sManifests kubernetes-admin@kubernetes
+
+set -e
+
+CONTEXT="$1"
+
+if [[ -z ${CONTEXT} ]]; then
+  echo "Usage: $0 KUBE-CONTEXT"
+  exit 1
+fi
+
+#NAMESPACES=$(kubectl --context ${CONTEXT} get -o json namespaces|jq '.items[].metadata.name'|sed "s/\"//g")
+
+RESOURCES="configmap secret daemonset deployment service hpa statefulset serviceaccount configmap clusterrole rolebinding role"
+
+#RESOURCES="statefulset"
+
+NAMESPACE="prometheus"
+
+for ns in ${NAMESPACE};do
+  for resource in ${RESOURCES};do
+    rsrcs=$(kubectl --context ${CONTEXT} -n ${ns} get -o json ${resource}|jq '.items[].metadata.name'|sed "s/\"//g")
+    for r in ${rsrcs}
+    do
+      r2=$(echo $r | sed 's/:/_/g')
+      dir="${CONTEXT}/${ns}/${resource}"
+      mkdir -p "${dir}"
+      kubectl --context ${CONTEXT} -n ${ns} get -o yaml ${resource} ${r} > "${dir}/${r2}.yaml"
+    done
+  done
+done
